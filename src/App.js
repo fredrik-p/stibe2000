ccacd13cd678f57c599e62c0608f0bb70aa61bc9
Fred P <58442519+fredrik-p@users.noreply.github.com>
2022-06-29 09:36:56 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h1">DevALMO, YouTube Channel</Typography>
+    </>
+  );
+}
+
+export default App;
